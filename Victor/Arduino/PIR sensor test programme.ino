f7083a60519fff3a17e03799447da61433a160a3
hoegyv <victor.hoegy@hva.nl>
2025-02-20 11:45:21 +0100

    PIR sensor test programme

---
+++ Victor/Arduino/PIR sensor test programme.ino
@@ -0,0 +1,17 @@
+const int pirPin = 2;
+
+void setup() {
+    pinMode(pirPin, INPUT);
+    Serial.begin(9600);
+
+void loop() {
+    int motionState = digitalRead(pirPin);
+    
+    if (motionState == HIGH) {
+        Serial.println("Mouvement détecté !");
+    } else {
+        Serial.println("Aucun mouvement");
+    }
+    
+    delay(500);
+}
