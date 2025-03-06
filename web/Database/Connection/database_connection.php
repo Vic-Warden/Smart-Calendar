0f536c93b48a8b8fdb795e9a6ae43b1c3f2c4738
hoegyv <victor.hoegy@hva.nl>
2025-03-06 18:49:26 +0100

    Automisation for ip adress cancel

--- web/Database/Connection/database_connection.php
+++ web/Database/Connection/database_connection.php
@@ -8,16 +8,6 @@ $link = mysqli_connect($host, $user, $password, $database);
 
 if (!$link) 
 {
-    die('Emperor forgive me, I have failed: ' . mysqli_connect_error());
+    die('Emperor forgive me, I have failed' . mysqli_connect_error());
 }
-
-$server_ip = gethostbyname(gethostname());
-
-$data = 
-[
-    "server_ip" => $server_ip,
-    "timestamp" => date("Y-m-d H:i:s")
-];
-
-file_put_contents(__DIR__ . "/server_ip.json", json_encode($data, JSON_PRETTY_PRINT));
 ?>
