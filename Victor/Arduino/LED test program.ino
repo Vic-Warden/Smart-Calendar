9a7fbd7ba9acc24c36e4e02d4db21ed58e3b0e88
hoegyv <victor.hoegy@hva.nl>
2025-03-06 14:03:51 +0100

    LED Upload for save some pins on the wemos

--- Victor/Arduino/LED test program.ino
+++ Victor/Arduino/LED test program.ino
@@ -1,19 +1,12 @@
-#define LED1 4  
-#define LED2 5  
+#define LED_PIN 5 // define the LED pin
 
-void setup() 
-{
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
+void setup() {
+  pinMode(LED_PIN, OUTPUT); // set the LED pin as an output
 }
 
-void loop() 
-{
-  digitalWrite(LED1, HIGH); 
-  digitalWrite(LED2, LOW);  
-  delay(1000);              
-
-  digitalWrite(LED1, LOW); 
-  digitalWrite(LED2, HIGH); 
-  delay(1000);              
+void loop() {
+  digitalWrite(LED_PIN, HIGH); // turn the LED on
+  delay(1000); // wait for 1 second
+  digitalWrite(LED_PIN, LOW); // turn the LED off
+  delay(1000); // wait for 1 second
 }
