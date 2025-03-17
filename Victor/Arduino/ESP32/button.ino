33943ea93a9268fe474d62195cefc9f50f2d56e5
hoegyv <victor.hoegy@hva.nl>
2025-03-17 17:20:25 +0100

    Button connected to the ESP32

---
+++ Victor/Arduino/ESP32/button.ino
@@ -0,0 +1,21 @@
+const int boutonPin = 14;
+bool boutonAppuye = false;
+
+void setup() {
+    pinMode(boutonPin, INPUT_PULLUP);
+    Serial.begin(115200);
+}
+
+void loop() {
+    if (digitalRead(boutonPin) == LOW) {
+        boutonAppuye = true;
+    }
+
+    if (boutonAppuye && digitalRead(boutonPin) == HIGH) {
+        Serial.println("Clic détecté !");
+        boutonAppuye = false;
+        delay(200);
+    }
+
+    delay(10);
+}
